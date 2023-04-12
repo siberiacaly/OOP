fa5d6048200af64242b5a2912e35f419755ab527
Tomáš Calábek <124521380+siberiacaly@users.noreply.github.com>
2023-04-12 15:54:49 +0200

    Add files via upload

---
+++ cv10/SQLQuery1.sql
@@ -0,0 +1,31 @@
+﻿DBCC CHECKIDENT([Students],RESEED, 0)
+SELECT * FROM [Students]
+INSERT INTO Students VALUES ('Jan', 'Kozoroh', '2002-01-19')
+INSERT INTO Students VALUES ('Eva', 'Vodnarova', '2003-02-09')
+INSERT INTO Students VALUES ('Dan', 'Ryba', '2000-03-01')
+INSERT INTO Students VALUES ('Lukas', 'Beran', '2006-04-25')
+INSERT INTO Students VALUES ('Alena', 'Bycivarle', '2008-05-15')
+INSERT INTO Students VALUES ('Pavel', 'Strelec', '2001-12-07')
+
+
+DBCC CHECKIDENT([Subjects],RESEED, 0)
+SELECT * FROM Subjects
+INSERT INTO Subjects VALUES ('BPC-OOP', 'Object-Oriented Programming')
+INSERT INTO Subjects VALUES ('BPC-MA3', 'Mathematics 2')
+INSERT INTO Subjects VALUES ('BPC-PC2T', 'Computers and Programming 2')
+
+
+
+SELECT * FROM Student_subject
+INSERT INTO Student_subject VALUES (0,1, '2021-04-14', 1)
+INSERT INTO Student_subject VALUES (1,1, '2021-04-14', 3)
+INSERT INTO Student_subject VALUES (3,1, '2021-04-14', 5)
+
+INSERT INTO Student_subject VALUES (2,2, '2022-04-14', 1)
+INSERT INTO Student_subject VALUES (3,2, '2022-04-14', 2)
+INSERT INTO Student_subject VALUES (4,2, '2022-04-14', 3)
+INSERT INTO Student_subject VALUES (5,2, '2022-04-14', 4)
+
+INSERT INTO Student_subject VALUES (4,3, '2023-04-14', 2)
+INSERT INTO Student_subject VALUES (5,3, '2023-04-14', 6)
+
---
+++ cv10/SQLQuery2.sql
@@ -0,0 +1,18 @@
+﻿SELECT firstname as [First name], secondname as [Second name], born_date as [Born], shortcut, subject_name as [Name of subject] FROM Students st LEFT JOIN
+Student_subject ss ON st.studentID = ss.studentID LEFT JOIN Subjects sb ON sb.subjectID = ss.subjectId
+
+
+SELECT secondname as [Second name] , count(secondname) as ocurencies FROM Students GROUP BY secondname
+
+
+SELECT subject_name as [Name of subject], count(Student_subject.subjectId) as ocurencies 
+FROM Student_subject 
+LEFT JOIN Subjects st ON Student_subject.subjectId = st.subjectID 
+GROUP BY subject_name HAVING COUNT(Student_subject.studentID) < 3
+
+SELECT subject_name as [Name of subject], MIN(NULLIF(mark,0)) as [Best mark], MAX(mark) as [Worst mark], 
+CAST(AVG(CAST(NULLIF(mark,0) AS DECIMAL(10,2))) AS DECIMAL(10,2)) as [Average mark],
+count(NULLIF(mark,0)) as [Number of marked students]
+FROM Student_subject 
+LEFT JOIN Subjects st ON Student_subject.subjectId = st.subjectID
+GROUP BY subject_name
